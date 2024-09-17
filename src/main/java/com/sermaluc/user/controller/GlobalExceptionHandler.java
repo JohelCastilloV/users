82e7dd09a03cceed256e2169a412b861d0f3ff72
Johel Castillo <johel@Johels-MacBook-Pro.local>
2024-09-17 13:21:13 -0500

    add exception handler

--- src/main/java/com/sermaluc/user/controller/GlobalExceptionHandler.java
+++ src/main/java/com/sermaluc/user/controller/GlobalExceptionHandler.java
@@ -1,4 +1,31 @@
 package com.sermaluc.user.controller;
 
+import com.sermaluc.user.exception.GlobalException;
+import com.sermaluc.user.exception.UserAlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Objects;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<GlobalException> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        GlobalException globalException = new GlobalException();
+        globalException.setMensaje(ex.getMessage());
+        return new ResponseEntity<>(globalException, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        GlobalException globalException = new GlobalException();
+        if(Objects.nonNull(ex.getFieldError())){
+            globalException.setMensaje(ex.getFieldError().getDefaultMessage());
+        }else {
+            globalException.setMensaje(ex.getMessage());
+        }
+        return new ResponseEntity<>(globalException, HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/sermaluc/user/exception/GlobalException.java
+++ src/main/java/com/sermaluc/user/exception/GlobalException.java
@@ -1,4 +1,8 @@
 package com.sermaluc.user.exception;
 
+import lombok.Data;
+
+@Data
 public class GlobalException {
+    private String mensaje;
 }
